153085baadd91361f20b3ec8a970e3d41cf424ee
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2022-12-01 13:03:54 +0100

    add function to sum numbers in a string sep by \n

--- src/helpers.rs
+++ src/helpers.rs
@@ -2,3 +2,15 @@
  * Use this file if you want to extract helpers from your solutions.
  * Example import from this file: `use advent_of_code::helpers::example_fn;`.
  */
+
+pub fn sum_list(input: &str) -> u32 {
+
+    let nums = input.split("\n");
+    let mut sum: u32 = 0;
+    for n in nums{
+        if !n.is_empty() {
+            sum += n.parse::<u32>().unwrap();
+        }
+    }
+    sum
+}
