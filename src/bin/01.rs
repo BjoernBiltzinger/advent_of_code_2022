f3aa6d04c2043bf2edb9e1c1479017c540d22006
BjoernBiltzinger <bjoernbiltzinger@gmx.de>
2022-12-02 10:54:12 +0100

    test against example not real input

--- src/bin/01.rs
+++ src/bin/01.rs
@@ -39,13 +39,13 @@ mod tests {
 
     #[test]
     fn test_part_one() {
-        let input = &advent_of_code::read_file("inputs", 1);
-        assert_eq!(part_one(&input), Some(67633));
+        let input = &advent_of_code::read_file("examples", 1);
+        assert_eq!(part_one(&input), Some(24000));
     }
 
     #[test]
     fn test_part_two() {
-        let input = &advent_of_code::read_file("inputs", 1);
-        assert_eq!(part_two(&input), Some(199628));
+        let input = &advent_of_code::read_file("examples", 1);
+        assert_eq!(part_two(&input), Some(45000));
     }
 }
